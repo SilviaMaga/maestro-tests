15b501685101d0c1ec5425289872c248cb04b1be
Silvia Magalhães <93656249+SilviaMaga@users.noreply.github.com>
2024-08-23 12:36:46 -0300

    first commit

---
+++ e2e/elements/login.js
@@ -0,0 +1,8 @@
+// login.js
+output.login = {
+    email: 'email',
+    password: 'password',
+    loginBtn: 'login-button',
+    emailLabel: 'E-mail',
+    passwordLabel: 'Senha'
+}
---
+++ e2e/elements/pedido.js
@@ -0,0 +1,12 @@
+output.pedido = {
+    selectAddress: 'Selecione seu endereço...',
+    alertTitle: 'android:id/alertTitle',
+    alertButton: 'android:id/button1',
+    restaurantName: 'Pastelaria da Maria',
+    firstItemTitle: 'item-title',
+    cartButton: 'open-cart-button',
+    confirmOrderButton: 'confirm-order-button',
+    paymentOption: 'Dinheiro',
+    finalizeOrderButton: 'do-order-button',
+    orderSuccessMessage: 'Pedido realizado'
+}
